f61eaee07e57692a6c2d8d344085d8dfa4098017
xyugen <alex.arias.jorym@gmail.com>
2023-03-30 20:28:38 +0800

    feat(fish): add different themes and add aliases to edit configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 fish_add_path /home/yugen/.spicetify
+
+alias nvimrc='nvim ~/.config/nvim/init.vim'
+alias fishrc='nvim ~/.config/fish/config.fish'
